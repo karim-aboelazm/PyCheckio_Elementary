cfe23d4cd53189e227b49c51e55efb53055499fd
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2021-08-17 01:57:10 +0200

    first commit

---
+++ Replace First/mission.py
@@ -0,0 +1,22 @@
+from typing import Iterable
+
+
+def replace_first(items: list) -> Iterable:
+    if len(items) != 0:
+        items.append(items[0])
+        items.insert(1,items[-1])
+        items.remove(items[0])
+        items.remove(items[-1])
+    return items
+
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(list(replace_first([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    assert list(replace_first([1])) == [1]
+    assert list(replace_first([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
